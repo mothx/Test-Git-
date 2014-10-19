3105a204678e8b01a02b77e3c0a1d83cdcb5bed5
mothx <self.felix@gmail.com>
2014-10-19 18:56:28 +0200

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Felix Bengtsson on 19/10/14.
+//  Copyright (c) 2014 Felix Bengtsson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Felix Bengtsson on 19/10/14.
+//  Copyright (c) 2014 Felix Bengtsson. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
